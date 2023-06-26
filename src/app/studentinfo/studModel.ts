110e74020616f0bbabb81858351a024f01254f0e
Soujanaya Sambanni <soujanayasambannibldea@gmail.com>
2023-06-26 12:56:21 +0530

    student

---
+++ src/app/studentinfo/studModel.ts
@@ -0,0 +1,18 @@
+export interface studModel{
+    name:string;
+    selectName:string;
+    dob:number;
+    usn:string;
+    mname:string;
+    fname:string;
+    snumber:number;
+    pnumber:number;
+    profile:string;
+    course:string;
+    id:number;
+    
+    
+    
+     
+     
+ }
